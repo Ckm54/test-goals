a815a5f25d3dfa5632d1df1c2d5621222d671424
Collo <mutaic84@gmail.com>
2022-03-31 00:32:57 +0300

    add dates to each goal

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -16,7 +16,7 @@ export class GoalComponent implements OnInit {
   goals:Goal[] = [
     new Goal(1, "Watch finding Nemo", "Find an online version and watch merlin fing his son", new Date(2020, 3, 14)),
     new Goal(2, "Buy Cookies", "I have to buy cookies for the parrot", new Date(2019, 6, 9)),
-    new Goal(3, "Get a New Phone Case", "Diana has her birthday coming soon!", new Date(2022, 1, 12)),
+    new Goal(3, "Get a New Phone Case", "Diana has her birthday coming soon!", new Date(2022, 7, 12)),
     new Goal(4, "Get Dog Food", "Puppy likes some expensive tasty food", new Date(2019, 0, 18)),
     new Goal(5, "Finish up creating application ui", "Damn another application!", new Date(2019, 2, 14)),
     new Goal(6, "plot my world domination plan", "Because i am an evil overload!!!", new Date(2022, 3, 14)),
