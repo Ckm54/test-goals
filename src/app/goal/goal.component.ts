38142455417db12bc6952725c47676336185cfb6
Collo <mutaic84@gmail.com>
2022-03-31 00:02:29 +0300

    create a goals array and functions to toggle and handle completed goals -- delete a completed goal

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal';
 
 @Component({
   selector: 'app-goal',
@@ -10,6 +11,25 @@ export class GoalComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  goals:Goal[] = [
+    new Goal(1, "Watch finding Nemo", "Find an online version and watch merlin fing his son", new Date(2020, 3, 14)),
+    new Goal(2, "Buy Cookies", "I have to buy cookies for the parrot", new Date(2019, 6, 9)),
+    new Goal(3, "Get a New Phone Case", "Diana has her birthday coming soon!", new Date(2022, 1, 12)),
+    new Goal(4, "Get Dog Food", "Puppy likes some expensive tasty food", new Date(2019, 0, 18)),
+    new Goal(5, "Finish up creating application ui", "Damn another application!", new Date(2019, 2, 14)),
+    new Goal(6, "plot my world domination plan", "Because i am an evil overload!!!", new Date(2022, 3, 14)),
+  ]
+
+  toggleDetails(index: number) {
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+
+  completeGoal(isComplete: boolean, index: number) {
+    if (isComplete) {
+      this.goals.splice(index, 1)
+    }
   }
 
 }
